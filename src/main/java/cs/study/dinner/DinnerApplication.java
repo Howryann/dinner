de971f84a49dd9118829316bc629b89d918acb57
howryann <hy301250@163.com>
2022-06-24 16:57:10 +0800

    yml文件

---
+++ src/main/java/cs/study/dinner/DinnerApplication.java
@@ -0,0 +1,14 @@
+package cs.study.dinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Howryann
+ */
+@SpringBootApplication
+public class DinnerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DinnerApplication.class,args);
+    }
+}
